7ae5f1496da5fcc33e9512aca62a33b1405c1cb9
GitHub4Eddy <68952054+GitHub4Eddy@users.noreply.github.com>
2021-07-30 16:39:29 +0200

    Create solaredge_monitor_10.lua

---
+++ solaredge_monitor_10.lua
@@ -0,0 +1,454 @@
+-- QUICKAPP SolarEdge Monitor
+
+-- This QuickApp monitors your SolarEdge managed Solar Panels
+-- The QuickApp has (child) devices for currentPower, lastDayData, lastMonthData, lastYearData and lifeTimeData
+-- See API documentation on https://www.solaredge.com/sites/default/files/se_monitoring_api.pdf 
+
+
+-- Changes version 1.0 (30th July 2021)
+-- Total m² solar panels added to log text and label text
+-- Check for API existance of lifeTimeData_revenue (not available in older firmware)
+-- Automatic conversion added for lastmonthData, lastYearData and lifetimeData production to kWh, MWh or GWh
+-- Automatic conversion added for currentPower to Watt, Kilowatt, Megawatt or Gigawatt (yes, Megawatt and Gigawatt is optimistic)
+-- Get the Peakpower and Currency settings from Inverter
+-- Peakpower added to label text and child device log text
+
+-- Changes version 0.3 (12th April 2021)
+-- Added last update date / time
+-- Cleaned up the code
+
+-- Changes version 0.2 (12th April 2021)
+-- Disabled revenue value (except for lifeTimeData)
+
+-- Changes version 0.1 (11th April 2021)
+-- First (test) version
+
+
+-- Variables (mandatory): 
+-- siteID = ID of your SolarEdge Monitor
+-- apiKey = API key of your SolarEdge Monitor
+-- solarM2 = The amount of m2 Solar Panels (use . for decimals) for calculating Solar Power m2 (default = 0)
+-- interval = The daily API limitiation is 300 requests. The default request interval is 360 seconds (6 minutes).
+-- debugLevel = Number (1=some, 2=few, 3=all, 4=simulation mode) (default = 1)
+-- icon = User defined icon number (add the icon via an other device and lookup the number) (default = 0)
+
+
+-- Example json overview string from API documentation:
+-- {"overview": {"lastUpdateTime": "2021-03-01 02:37:47","lifeTimeData": {"energy": 761985.75,"revenue": 946.13104},"lastYearData": {"energy": 761985.8,"revenue": 946.13104},"lastMonthData": {"energy": 492736.7,"revenue": 612.09528},"lastDayData": {"energy": 1327.3,"revenue": 1.64844},"currentPower": {"power": 304.8}}}
+
+-- Example json overview string from SolarEdge response:
+-- {"overview":{"lastUpdateTime":"2021-04-12 13:46:04","lifeTimeData":{"energy":7827878.0,"revenue":1728.5211},"lastYearData":{"energy":573242.0},"lastMonthData":{"energy":113386.0},"lastDayData":{"energy":7373.0},"currentPower":{"power":134.73499},"measuredBy":"INVERTER"}}
+
+
+-- No editing of this code is needed 
+
+
+class 'currentPower'(QuickAppChild)
+function currentPower:__init(dev)
+  QuickAppChild.__init(self,dev)
+  --self:trace("currentPower QuickappChild initiated, deviceId:",self.id)
+end
+function currentPower:updateValue(data) 
+  self:updateProperty("value", tonumber(data.currentPower))
+  self:updateProperty("unit", data.currentPowerUnit)
+  self:updateProperty("log", details.peakPower .." kWp")
+end
+
+class 'solarPower'(QuickAppChild)
+function solarPower:__init(dev)
+  QuickAppChild.__init(self,dev)
+  --self:trace("solarPower QuickappChild initiated, deviceId:",self.id)
+end
+function solarPower:updateValue(data) 
+  self:updateProperty("value", tonumber(data.solarPower))
+  self:updateProperty("unit", "Watt/m²")
+  self:updateProperty("log", solarM2 .." m²")
+end
+
+class 'lastDayData'(QuickAppChild)
+function lastDayData:__init(dev)
+  QuickAppChild.__init(self,dev)
+  --self:trace("lastDayData QuickappChild initiated, deviceId:",self.id)
+end
+function lastDayData:updateValue(data) 
+  self:updateProperty("value", tonumber(data.lastDayData))
+  self:updateProperty("unit", data.lastDayUnit)
+  self:updateProperty("log", "")
+end
+
+class 'lastMonthData'(QuickAppChild)
+function lastMonthData:__init(dev)
+  QuickAppChild.__init(self,dev)
+  --self:trace("lastMonthData QuickappChild initiated, deviceId:",self.id)
+end
+function lastMonthData:updateValue(data) 
+  self:updateProperty("value", tonumber(data.lastMonthData))
+  self:updateProperty("unit", data.lastMonthUnit)
+  self:updateProperty("log", "")
+end
+
+class 'lastYearData'(QuickAppChild)
+function lastYearData:__init(dev)
+  QuickAppChild.__init(self,dev)
+  --self:trace("lastYearData QuickappChild initiated, deviceId:",self.id)
+end
+function lastYearData:updateValue(data) 
+  self:updateProperty("value", tonumber(data.lastYearData))
+  self:updateProperty("unit", data.lastYearUnit)
+  self:updateProperty("log", "")
+end
+
+class 'lifeTimeData'(QuickAppChild)
+function lifeTimeData:__init(dev)
+  QuickAppChild.__init(self,dev)
+  --self:trace("lifeTimeData QuickappChild initiated, deviceId:",self.id)
+end
+function lifeTimeData:updateValue(data) 
+  self:updateProperty("value", tonumber(data.lifeTimeData))
+  self:updateProperty("unit", data.lifeTimeUnit)
+  self:updateProperty("log", data.lifeTimeData_revenue)
+end
+
+
+local function getChildVariable(child,varName)
+  for _,v in ipairs(child.properties.quickAppVariables or {}) do
+    if v.name==varName then return v.value end
+  end
+  return ""
+end
+
+
+-- QuickApp functions
+
+
+function QuickApp:logging(level,text) -- Logging function for debug
+  if tonumber(debugLevel) >= tonumber(level) then 
+      self:debug(text)
+  end
+end
+
+
+function QuickApp:solarPower(power, m2) -- Calculate Solar Power M2
+  self:logging(3,"Start solarPower")
+  if m2 > 0 and power > 0 then
+    solarPower = power / m2
+  else
+    solarPower = 0
+  end
+  return solarPower
+end
+
+
+function QuickApp:unitCheckWatt(measurement) -- Set the measurement and unit to Watt, Kilowatt, Megawatt or Gigawatt
+  self:logging(3,"Start unitCheckWatt")
+  if measurement > 1000000000 then
+    return string.format("%.2f",measurement/1000000000),"Gigawatt"
+  elseif measurement > 1000000 then
+    return string.format("%.2f",measurement/1000000),"Megawatt"
+  elseif measurement > 1000 then
+    return string.format("%.2f",measurement/1000),"Kilowatt"
+  else
+    return string.format("%.2f",measurement),"Watt"
+  end
+end
+
+
+function QuickApp:unitCheckWh(measurement) -- Set the measurement and unit to kWh, MWh or GWh
+  self:logging(3,"Start unitCheckWh")
+  if measurement > 1000000000 then
+    return string.format("%.2f",measurement/1000000000),"GWh"
+  elseif measurement > 1000000 then
+    return string.format("%.2f",measurement/1000000),"MWh"
+  elseif measurement > 1000 then
+    return string.format("%.2f",measurement/1000),"kWh"
+  else
+    return string.format("%.2f",measurement),"Wh"
+  end
+end
+
+
+function QuickApp:updateProperties() -- Update the properties
+  self:logging(3,"updateProperties")
+  self:updateProperty("value", tonumber(data.currentPower))
+  self:updateProperty("unit", data.currentPowerUnit)
+  self:updateProperty("log", data.lastUpdateTime)
+end
+
+
+function QuickApp:updateLabels() -- Update the labels
+  self:logging(3,"updateLabels")
+  local labelText = ""
+  labelText = labelText .."Current power: " ..data.currentPower .." " ..data.currentPowerUnit .."\n\n"
+  labelText = labelText .."Peakpower: " ..details.peakPower .." kWp" .."\n"
+  labelText = labelText .."Solar power: " ..data.solarPower .." Watt/m² (" ..solarM2 .." m²)" .."\n"
+  labelText = labelText .."Lastday: " ..data.lastDayData .." " ..data.lastDayUnit .."\n"
+  labelText = labelText .."Lastmonth: " ..data.lastMonthData .." " ..data.lastMonthUnit .."\n"
+  labelText = labelText .."Lastyear: " ..data.lastYearData .." " ..data.lastYearUnit .."\n"
+  labelText = labelText .."Lifetime: " ..data.lifeTimeData .." " ..data.lifeTimeUnit .." (" ..data.lifeTimeData_revenue ..")" .."\n\n"
+  labelText = labelText .."Last update: " ..data.lastUpdateTime .."\n" 
+  self:updateView("label1", "text", labelText)
+  self:logging(2,labelText)
+end
+
+
+function QuickApp:valuesOverview() -- Get the values from json file Overview
+  self:logging(3,"valuesOverview")
+  data.currentPower = string.format("%.0f", jsonTable.overview.currentPower.power)
+  data.solarPower = string.format("%.2f",self:solarPower(tonumber(data.currentPower), tonumber(solarM2)))
+  data.currentPower, data.currentPowerUnit = self:unitCheckWatt(tonumber(data.currentPower)) -- Set measurement and unit to Watt, Kilowatt, Megawatt og Gigawatt
+  data.lastDayData = string.format("%.2f", jsonTable.overview.lastDayData.energy)
+  data.lastDayData, data.lastDayUnit = self:unitCheckWh(tonumber(data.lastDayData)) -- Set measurement and unit to kWh, MWh or GWh
+  data.lastMonthData = string.format("%.2f", jsonTable.overview.lastMonthData.energy)
+  data.lastMonthData, data.lastMonthUnit = self:unitCheckWh(tonumber(data.lastMonthData)) -- Set measurement and unit to kWh, MWh or GWh
+  data.lastYearData = string.format("%.2f", jsonTable.overview.lastYearData.energy)
+  data.lastYearData, data.lastYearUnit = self:unitCheckWh(tonumber(data.lastYearData)) -- Set measurement and unit to kWh, MWh or GWh
+  data.lifeTimeData = string.format("%.2f", jsonTable.overview.lifeTimeData.energy)
+  data.lifeTimeData, data.lifeTimeUnit = self:unitCheckWh(tonumber(data.lifeTimeData)) -- Set measurement and unit to kWh, MWh or GWh
+  if data.lifeTimeData_revenue ~= nil then -- lifeTimeData_revenue is not mandatory
+    data.lifeTimeData_revenue = string.format("%.2f", jsonTable.overview.lifeTimeData.revenue) .." " ..details.currency
+  end
+  data.lastUpdateTime = jsonTable.overview.lastUpdateTime
+end
+
+
+function QuickApp:valuesDetails() -- Get the values from json file Details
+  self:logging(3,"valuesDetails")
+  if details.peakPower ~= nil then -- details.peakPower is not mandatory
+    details.peakPower = string.format("%.1f", jsonTableDetails.details.peakPower)
+  end
+  if details.currency ~= nil then -- details.currency is not mandatory
+    details.currency = jsonTableDetails.details.currency
+  end
+end
+
+
+function QuickApp:simData() -- Simulate SolarEdge Monitor
+  self:logging(3,"Simulation mode")
+  apiResult = '{"overview":{"lastUpdateTime":"2021-04-12 13:46:04","lifeTimeData":{"energy":7827878.0,"revenue":1728.5211},"lastYearData":{"energy":573242.0},"lastMonthData":{"energy":113386.0},"lastDayData":{"energy":7373.0},"currentPower":{"power":134.73499},"measuredBy":"INVERTER"}}'
+ 
+  jsonTable = json.decode(apiResult) -- Decode the json string from api to lua-table 
+  
+  self:valuesOverview()
+  self:updateLabels()
+  self:updateProperties()
+
+  for id,child in pairs(self.childDevices) do 
+    child:updateValue(data,userID) 
+  end
+  
+  self:logging(3,"SetTimeout " ..interval .." seconds")
+  fibaro.setTimeout(interval*1000, function() 
+     self:simData()
+  end)
+end
+
+
+function QuickApp:getData() -- Get the data from the API
+  self:logging(3,"getData")
+  self:logging(2,"URL Overview: " ..urlOverview)
+  http:request(urlOverview, {
+    options={headers = {Accept = "application/json"},method = 'GET'},   
+      success = function(response)
+        self:logging(3,"response status: " ..response.status)
+        self:logging(3,"headers: " ..response.headers["Content-Type"])
+        self:logging(2,"Response data: " ..response.data)
+
+        if response.data == nil or response.data == "" or response.data == "[]" then -- Check for empty result
+          self:warning("Temporarily no production data from SolarEdge Monitor")
+          self:logging(3,"SetTimeout " ..interval .." seconds")
+          fibaro.setTimeout(interval*1000, function() 
+            self:getdata()
+          end)
+        end
+        
+        jsonTable = json.decode(response.data) -- JSON decode from api to lua-table
+
+        self:valuesOverview()
+        self:updateLabels()
+        self:updateProperties()
+
+        for id,child in pairs(self.childDevices) do 
+          child:updateValue(data,userID) 
+        end
+
+      end,
+      error = function(error)
+        self:error("error: " ..json.encode(error))
+        self:updateProperty("log", "error: " ..json.encode(error))
+      end
+    }) 
+  
+  self:logging(3,"SetTimeout " ..interval .." seconds")
+  fibaro.setTimeout(interval*1000, function() 
+     self:getData()
+  end)
+end
+
+
+function QuickApp:getDetails() -- Get the settings from the API
+  self:logging(3,"getDetails")
+  self:logging(2,"URL Details: " ..urlDetails)
+  http:request(urlDetails, {
+    options={headers = {Accept = "application/json"},method = 'GET'},   
+      success = function(response)
+        self:logging(3,"response status: " ..response.status)
+        self:logging(3,"headers: " ..response.headers["Content-Type"])
+        self:logging(2,"Response data: " ..response.data)
+
+        if response.data == nil or response.data == "" or response.data == "[]" then -- Check for empty result
+          self:warning("Temporarily no details data from SolarEdge Monitor")
+          --self:logging(3,"SetTimeout " ..interval .." seconds")
+          --fibaro.setTimeout(interval*1000, function() 
+            --self:getDetails()
+          --end)
+        else
+          jsonTableDetails = json.decode(response.data) -- JSON decode from api to lua-table
+        end
+
+        self:valuesDetails()
+
+      end,
+      error = function(error)
+        self:error("error: " ..json.encode(error))
+        self:updateProperty("log", "error: " ..json.encode(error))
+      end
+    }) 
+end
+
+
+function QuickApp:createVariables() -- Create all Variables 
+  jsonTable = {}
+  jsonTableDetails = {}
+  details = {}
+  details.peakPower = "0"
+  details.currency = "EUR"
+  data = {}
+  data.currentPower = "0"
+  data.currentPowerUnit = "Watt"
+  data.solarPower = "0" 
+  data.lastDayData = "0"
+  data.lastDayUnit = "Wh"
+  data.lastMonthData = "0"
+  data.lastMonthUnit = "Wh"
+  data.lastYearData= "0"
+  data.lastYearUnit= "Wh"
+  data.lifeTimeData= "0"
+  data.lifeTimeUnit= "Wh"
+  data.lifeTimeData_revenue = "0"
+  data.lastUpdateTime = ""
+end
+
+
+function QuickApp:getQuickAppVariables() -- Get all Quickapp Variables or create them
+  local siteID = self:getVariable("siteID")
+  local apiKey = self:getVariable("apiKey")
+  solarM2 = tonumber(self:getVariable("solarM2"))
+  interval = tonumber(self:getVariable("interval")) 
+  httpTimeout = tonumber(self:getVariable("httpTimeout")) 
+  debugLevel = tonumber(self:getVariable("debugLevel"))
+  local icon = tonumber(self:getVariable("icon")) 
+
+  -- Check existence of the mandatory variables, if not, create them with default values
+  if siteID == "" or siteID == nil then
+    siteID = "1" -- This siteID is just an example, it is not working 
+    self:setVariable("siteID",siteID)
+    self:trace("Added QuickApp variable siteID")
+  end
+ if apiKey == "" or apiKey == nil then
+    apiKey = "L4QLVQ1LOKCQX2193VSEICXW61NP6B1O" -- This API key is just an example, it is not working
+    self:setVariable("apiKey",apiKey)
+    self:trace("Added QuickApp variable apiKey")
+  end 
+  if solarM2 == "" or solarM2 == nil then 
+    solarM2 = "0" 
+    self:setVariable("solarM2",solarM2)
+    self:trace("Added QuickApp variable solarM2")
+  end 
+  if interval == "" or interval == nil then
+    interval = "360" -- The default interval is 6 minutes (360 seconds) 
+    self:setVariable("interval",interval)
+    self:trace("Added QuickApp variable interval")
+    interval = tonumber(interval)
+  end
+  if httpTimeout == "" or httpTimeout == nil then
+    httpTimeout = "5" -- Default http timeout 
+    self:setVariable("httpTimeout",httpTimeout)
+    self:trace("Added QuickApp variable httpTimeout")
+    httpTimeout = tonumber(httpTimeout)
+  end 
+  if debugLevel == "" or debugLevel == nil then
+    debugLevel = "1" -- Default debug level
+    self:setVariable("debugLevel",debugLevel)
+    self:trace("Added QuickApp variable debugLevel")
+    debugLevel = tonumber(debugLevel)
+  end
+  if icon == "" or icon == nil then 
+    icon = "0" -- Default no user defined icon 
+    self:setVariable("icon",icon)
+    self:trace("Added QuickApp variable icon")
+    icon = tonumber(icon)
+  end
+  if icon ~= 0 then 
+    self:updateProperty("deviceIcon", icon) -- Set user defined icon 
+  end
+  urlOverview = "https://monitoringapi.solaredge.com/site/"..siteID .."/overview.json?api_key="..apiKey
+  urlDetails = "https://monitoringapi.solaredge.com/site/"..siteID .."/details?api_key="..apiKey
+end
+
+
+function QuickApp:setupChildDevices()
+  local cdevs = api.get("/devices?parentId="..self.id) or {} -- Pick up all Child Devices
+  function self:initChildDevices() end -- Null function, else Fibaro calls it after onInit()...
+
+  if #cdevs == 0 then -- If no Child Devices, create them
+      local initChildData = { 
+        {className="currentPower", name="Current Power", type="com.fibaro.multilevelSensor", value=0},
+        {className="solarPower", name="Solar Power", type="com.fibaro.multilevelSensor", value=0},
+        {className="lastDayData", name="Lastday", type="com.fibaro.multilevelSensor", value=0},
+        {className="lastMonthData", name="Lastmonth", type="com.fibaro.multilevelSensor", value=0},
+        {className="lastYearData", name="Lastyear", type="com.fibaro.multilevelSensor", value=0},
+        {className="lifeTimeData", name="Lifetime", type="com.fibaro.multilevelSensor", value=0},
+      }
+    for _,c in ipairs(initChildData) do
+      local child = self:createChildDevice(
+        {name = c.name,
+          type=c.type,
+          value=c.value,
+          unit=c.unit,
+          initialInterfaces = {},
+        },
+        _G[c.className] -- Fetch class constructor from class name
+      )
+      child:setVariable("className",c.className)  -- Save class name so we know when we load it next time
+    end   
+  else 
+    for _,child in ipairs(cdevs) do
+      local className = getChildVariable(child,"className") -- Fetch child class name
+      local childObject = _G[className](child) -- Create child object from the constructor name
+      self.childDevices[child.id]=childObject
+      childObject.parent = self -- Setup parent link to device controller 
+    end
+  end
+end
+
+
+function QuickApp:onInit()
+  __TAG = fibaro.getName(plugin.mainDeviceId) .." ID:" ..plugin.mainDeviceId
+  self:debug("onInit") 
+  
+  self:setupChildDevices()
+  self:getQuickAppVariables() 
+  self:createVariables()
+  
+  http = net.HTTPClient({timeout=httpTimeout*1000})
+  
+  self:getDetails()
+  
+  if tonumber(debugLevel) >= 4 then 
+    self:simData() -- Go in simulation
+  else
+    self:getData() -- Get data from SolarEdge Monitor
+  end
+end
+
+-- EOF 
